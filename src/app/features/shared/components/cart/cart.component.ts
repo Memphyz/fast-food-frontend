1cfaecf1d4f3fc4644744e652e5f1d76b9b1d917
Lucas Ribeiro <lucas2001.789@gmail.com>
2022-06-30 12:05:06 -0300

    aplicando melhorias no modal de carrinho

--- src/app/features/shared/components/cart/cart.component.ts
+++ src/app/features/shared/components/cart/cart.component.ts
@@ -1,3 +1,4 @@
+import { Product } from './../../../../core/interfaces/product.interface';
 import { Cart } from './cart';
 import { Component } from '@angular/core';
 import { BsModalRef } from 'ngx-bootstrap/modal';
@@ -12,6 +13,10 @@ export class CartComponent {
   public cart = Cart.cart();
 
   constructor(public readonly modalRef: BsModalRef) {
+  }
 
+  public productTotalPrice(product: Product): number {
+    const totalAddictionals = product.additionals?.map((addictional => addictional.total)).reduce((acumulator, totalAddictionals): number => acumulator + totalAddictionals, 0)
+    return product.price + totalAddictionals;
   }
 }
--- src/app/features/shared/components/cart/cart.ts
+++ src/app/features/shared/components/cart/cart.ts
@@ -9,17 +9,19 @@ export class Cart {
     const productCopy = cloneDeep(product)
     productCopy.additionals = productCopy.additionals.filter((addictional): boolean => addictional.quantity > 0)
     cart.products.push(productCopy);
-    cart.total += total;
+    console.log(cart);
+    cart.total = cart.products.map((product) => product.price + product.additionals.map(addictional => addictional.total)?.reduce((acumulator, value) => acumulator + value, 0)).reduce((acumulator, total) => acumulator + total, 0);
+
     sessionStorage.setItem('cart', JSON.stringify(cart));
   }
 
   public static itemsLengthString(): string {
-    const cart: ICart = JSON.parse(sessionStorage.getItem('cart'));
+    const cart: ICart = JSON.parse(sessionStorage.getItem('cart') || '{}');
     return `"${(cart?.products?.length || '').toString()}"`;
   }
 
   public static cart(): ICart {
-    const cart: ICart = JSON.parse(sessionStorage.getItem('cart'));
+    const cart: ICart = JSON.parse(sessionStorage.getItem('cart') || '{}');
     return cart;
   }
 }
