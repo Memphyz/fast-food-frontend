72e6f22e30a297de4940a474054aa3c2f86267e7
Lucas Ribeiro <lucas2001.789@gmail.com>
2022-09-08 20:43:15 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   pixCode: '00020126330014BR.GOV.BCB.PIX0111507404578965204000053039865802BR5919Lucas Alves Ribeiro6009Sao Paulo62070503***63049DF9',
-  api: 'https://fast-food-backend.vercel.app/api/v1/'
+  api: 'https://fast-food-backend.vercel.app/api/v1'
 };
